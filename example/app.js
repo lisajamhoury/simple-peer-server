1a40969bfa017661b9a152b56329c0b089fc7943
lisajamhoury <lisajamhoury@gmail.com>
2021-07-19 12:27:25 -0400

    Remove express from server example.

--- example/app.js
+++ example/app.js
@@ -1,11 +1,7 @@
-const express = require('express');
-const app = express();
-
 const http = require('http');
-const server = http.createServer(app);
 const SimplePeerServer = require('simple-peer-server');
+
+const server = http.createServer();
 const spServer = new SimplePeerServer(server, true);
 
 server.listen(8081);
-
-module.exports = app;
